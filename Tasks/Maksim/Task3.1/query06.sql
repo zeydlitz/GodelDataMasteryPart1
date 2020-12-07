336b5b96e8c295b9e55b5d49a97a7daff2a55e49
zeydlitz <maksim.ivashkevich27@gmail.com>
2020-12-07 22:57:29 +0300

    change query6

--- Tasks/Maksim/Task3.1/query06.sql
+++ Tasks/Maksim/Task3.1/query06.sql
@@ -1,10 +1,14 @@
 --Query  6
 SELECT TOP (10)
        SOD.SalesOrderID,
-       SOD.UnitPrice TotalPrice
-FROM Sales.SalesOrderHeader SOH
-INNER JOIN Sales.SalesOrderDetail SOD
-        ON SOD.SalesOrderID = SOH.SalesOrderID
-WHERE YEAR(SOH.OrderDate) = 2011
-ORDER BY SOD.UnitPrice DESC
+       SUM(SOD.UnitPrice) TotalPrice
+FROM Sales.SalesOrderDetail SOD
+WHERE EXISTS(
+        SELECT *
+        FROM Sales.SalesOrderHeader SOH
+        WHERE SOH.SalesOrderID = SOD.SalesOrderID
+              AND
+              YEAR(SOH.OrderDate) = 2011)
+GROUP BY SOD.SalesOrderID
+ORDER BY TotalPrice DESC
 ;

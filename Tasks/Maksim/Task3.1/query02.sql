ca6d5966ba77a3870c599fca9f3b029dce21f8bd
zeydlitz <maksim.ivashkevich27@gmail.com>
2020-12-07 11:52:37 +0300

    fix: tabs

--- Tasks/Maksim/Task3.1/query02.sql
+++ Tasks/Maksim/Task3.1/query02.sql
@@ -1,5 +1,5 @@
 --Query  2
 SELECT MIN(emp.BirthDate) DOB_of_the_oldest,
-	   MAX(emp.BirthDate) DOB_of_the_youngest
+       MAX(emp.BirthDate) DOB_of_the_youngest
 FROM HumanResources.Employee emp
 ;
--- Tasks/Maksim/Task3.1/query03.sql
+++ Tasks/Maksim/Task3.1/query03.sql
@@ -1,5 +1,5 @@
 --Query  3
 SELECT AVG(emp.VacationHours)/24 AVG_vacation_days,
-	   AVG(emp.SickLeaveHours)/24 AVG_sick_days
+       AVG(emp.SickLeaveHours)/24 AVG_sick_days
 FROM HumanResources.Employee emp
 ;

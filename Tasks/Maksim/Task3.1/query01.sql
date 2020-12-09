f06b0a34de96abef320577c303a66fe9d3417b56
Max <maksimka.ivashkevich27@gmail.com>
2020-12-10 02:14:49 +0300

    preprocessing

--- Tasks/Maksim/Task3.1/query01.sql
+++ Tasks/Maksim/Task3.1/query01.sql
@@ -1,11 +1,7 @@
 --TASK  1
-SELECT COUNT(*) Number_of_people
-FROM HumanResources.Employee emp
-INNER JOIN HumanResources.EmployeeDepartmentHistory history
-        ON emp.BusinessEntityID = history.BusinessEntityID
-INNER JOIN HumanResources.Department D 
-        ON D.DepartmentID = history.DepartmentID
-WHERE emp.MaritalStatus='M'
-      AND
-      D.Name='Sales'
+SELECT SOH.CustomerID,
+       MAX(SOH.OrderDate) Max_Order_Date
+FROM Sales.SalesOrderHeader SOH
+GROUP BY SOH.CustomerID
+ORDER BY SOH.CustomerID
 ;

f6b278da4da0f95234eb808a3f173bf792ee7ede
Max <maksimka.ivashkevich27@gmail.com>
2020-12-14 01:20:21 +0300

    fix: q5

--- Tasks/Maksim/Task7/query05.sql
+++ Tasks/Maksim/Task7/query05.sql
@@ -1,7 +1,11 @@
 --Query 5
 DELETE FROM od
 FROM tempdb.dbo.Orders od
-INNER JOIN tempdb.dbo.Customers cu
-        ON od.custid = cu.CustomerID
-WHERE cu.CountryRegionCode = 'FR'
+INNER JOIN AdventureWorks2019.Sales.SalesOrderHeader SOH
+		ON SOH.SalesOrderID = od.orderid
+INNER JOIN AdventureWorks2019.Sales.Customer customer
+		ON customer.CustomerID = SOH.CustomerID
+INNER JOIN tempdb.dbo.Customers c2
+		ON customer.CustomerID = c2.CustomerID
+WHERE c2.CountryRegionCode = 'FR'
 ;

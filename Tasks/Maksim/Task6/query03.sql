b18eefa4e8dab68739c665cab47b23cab08e92dc
Max <maksimka.ivashkevich27@gmail.com>
2020-12-13 17:49:39 +0300

    fix: task6

--- Tasks/Maksim/Task6/query03.sql
+++ Tasks/Maksim/Task6/query03.sql
@@ -1,9 +1,34 @@
 --Query  3
+--cross apply in mssql 2000???
+--SELECT empid,
+--       custid,
+--       qty
+--FROM dbo.EmpCustOrders
+--CROSS APPLY (
+--            VALUES('A', A),('B', B),('C', C),('D', D)) AS C(custid, qty)
+--WHERE qty IS NOT NULL
 SELECT empid,
-       custid,
-       qty
+       'A' custid,
+       A AS qty
 FROM dbo.EmpCustOrders
-CROSS APPLY (
-            VALUES('A', A),('B', B),('C', C),('D', D)) AS C(custid, qty)
-WHERE qty IS NOT NULL
+WHERE A  IS NOT NULL
+UNION ALL
+SELECT empid,
+       'B' AS custid,
+       B AS qty
+FROM dbo.EmpCustOrders
+WHERE B IS NOT NULL
+UNION ALL
+SELECT empid,
+       'C' AS custid,
+       C AS qty
+FROM dbo.EmpCustOrders
+WHERE C IS NOT NULL
+UNION ALL
+SELECT empid,
+       'D' AS custid,
+       D AS qty
+FROM dbo.EmpCustOrders
+WHERE D IS NOT NULL
+ORDER BY empid, custid
 ;
--- Tasks/Maksim/Task6/query07.sql
+++ Tasks/Maksim/Task6/query07.sql
@@ -2,7 +2,7 @@
 SELECT CustomerID,
        SalesOrderID,
        SubTotal,
-       ISNULL(SubTotal - LAG(SubTotal) OVER(PARTITION BY CustomerID ORDER BY SalesOrderID) ,SubTotal)  diffprev,
-       ISNULL(SubTotal - LEAD(SubTotal) OVER(PARTITION BY CustomerID ORDER BY SalesOrderID),SubTotal) AS diffnext
+       ISNULL(SubTotal - LAG(SubTotal) OVER(PARTITION BY CustomerID ORDER BY SalesOrderID) ,SubTotal) diffprev,
+       ISNULL(SubTotal - LEAD(SubTotal) OVER(PARTITION BY CustomerID ORDER BY SalesOrderID),SubTotal) diffnext
 FROM Sales.SalesOrderHeader
 ;
